95249ea6a5530219ddfff68cec43eaf56511bfca
Mohd Rehan <reyanabidin169@gmail.com>
2023-07-23 16:59:17 +0530

    Final Update\Finish

--- app.py
+++ app.py
@@ -1,16 +1,45 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify, send_file, render_template
+from PIL import Image
+from helper import preprocess_image, serve_pil_image, tensor_to_image, load_model
+import tensorflow_addons as tfa
 
 app = Flask(__name__)
-app.debug = True
+
+@app.route('/')
+def hello():
+    return render_template("index.html")
+
+@app.route('/api/transform', methods=['POST'])
+def transform_image():
+    # Check if the request contains an image file
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image found'})
+
+    image_file = request.files['image']
+    
+    # Check if the file is an image
+    # if not is_image_file(image_file):
+    #     return jsonify({'error': 'Invalid file type. Only image files are allowed.'})
+
+    # Load the image using PIL
+    img = Image.open(image_file)
+    
+    # Perform your image transformation operations here
+    img = preprocess_image(img)
+
+    custom_objects = {'InstanceNormalization': tfa.layers.InstanceNormalization}
+    model_AB = load_model("C:\\Users\\asus\\Desktop\\CXR Translations trained models\\g_model_AtoB12500.h5", custom_objects=custom_objects)
+    pred = model_AB.predict(img)
+    img = tensor_to_image(pred)
+    # Return the transformed image as a response
+    return serve_pil_image(img)
 
 
-@app.route('/', methods=['GET'])
-def home():
-    return render_template('./index.html')
+def is_image_file(file):
+    allowed_extensions = {'png', 'jpg', 'jpeg', 'gif'}
+    return '.' in file.filename and \
+           file.filename.rsplit('.', 1)[1].lower() in allowed_extensions
 
 
-# @app.route('/',methods= ['POST'])
-# def predict():
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
--- helper.py
+++ helper.py
@@ -2,7 +2,12 @@ from numpy.random import randint
 from numpy import vstack
 from matplotlib import pyplot 
 import numpy as np
-
+from PIL import Image
+import io
+import numpy as np
+from flask import send_file
+from keras.utils import img_to_array
+import tensorflow as tf
 def select_sample(dataset, n_samples):
     	# choose random instances
 	ix = randint(0, dataset.shape[0], n_samples)
@@ -27,3 +32,33 @@ def show_plot(imagesX, imagesY1, imagesY2):
 		# title
 		pyplot.title(titles[i])
 	pyplot.show()
+
+
+
+def preprocess_image(img):
+    img = img.convert("RGB")
+    img = img.resize((256, 256), Image.LANCZOS)
+    img = img_to_array(img)
+    img /= 255.0  # Normalize pixel values to [0, 1]
+    tensor = tf.constant(img, dtype=np.float32)
+    tensor = tf.expand_dims(tensor, axis=0)
+    return tensor
+
+
+def load_model(path, custom_objects):
+    return tf.keras.models.load_model(path, custom_objects=custom_objects)
+
+def tensor_to_image(tensor):
+    # Convert the TensorFlow tensor to a PIL image
+    tensor = tf.squeeze(tensor, axis=0)
+    tensor = (tensor+1) * 127.5
+    tensor = tensor.numpy().astype('uint8')
+    image = Image.fromarray(tensor)
+    return image
+
+def serve_pil_image(pil_image):
+    # Serve the PIL image as a response
+    img_io = io.BytesIO()
+    pil_image.save(img_io, 'JPEG', quality=70)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
